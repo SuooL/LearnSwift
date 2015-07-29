4f636d2f1c4790a1825675535d846800cedabd6f
SuooL <1020935219@qq.com>
2015-07-29 19:54:29 +0800

    exercise01

---
+++ MySwiftLearn.playground/Contents.swift
@@ -0,0 +1,70 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let i = 1 + 1
+
+let m = 12.0 % 5
+
+let b = 10
+var a = b
+
+a = 3
+
+let (q , p) = (1, 2)
+
+print(q)
+
+// swift
+1 + 2 // 等于 3 
+5 - 3 // 等于 2 
+2 * 3 // 等于 6
+10.0 / 2.5 // 等于 4.0”
+
+
+"Hello" + "Swift"
+
+let dog: Character = "🐶"
+let cow: Character = "牛"
+
+"🐶" + "🐂"
+
+let three = 3
+let minusThree = -three
+
+let defaultColorName = "red"
+var userDefinedColorName:String?  //默认值为nil
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+//userDefinedColorName的值为空 ，所以colorNameToUse的值为`red`”
+
+0/1
+
+
+var strOpt: String? = "Hello World!"
+strOpt     //{Some "Hello World!"}
+strOpt!    //Hello World!
+print(strOpt)
+print(strOpt!)
+
+var strTest: String? = "Hello World!"
+if strTest != nil {
+    "not nil"
+} else {
+    "nil"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
