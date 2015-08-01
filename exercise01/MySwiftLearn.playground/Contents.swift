a6cf28b04aae9da73d2ffdf611c294de4ffde0e7
SuooL <1020935219@qq.com>
2015-08-01 11:22:00 +0800

    update

--- exercise01/MySwiftLearn.playground/Contents.swift
+++ exercise01/MySwiftLearn.playground/Contents.swift
@@ -1,6 +1,11 @@
 //: Playground - noun: a place where people can play
 
+// 值永远不会被隐式转换为其他类型。
+
+
 import UIKit
+
+// use let to make a constant and var to make a variable
 
 var str = "Hello, playground"
 let i = 1 + 1
@@ -40,6 +45,7 @@ var colorNameToUse = userDefinedColorName ?? defaultColorName
 
 0/1
 
+// 关于Optional类型
 
 var strOpt: String? = "Hello World!"
 strOpt     //{Some "Hello World!"}
@@ -55,8 +61,124 @@ if strTest != nil {
 }
 
 
+// 字典和列表
+var stuList = ["Hu", "SuooL", "C_s", "Dog"]
+
+stuList[1] = "Cow"
+
+stuList
+
+var stuDic = [
+    "Name" : "Hu",
+    "Sex" : "Male",
+    "Age" : "22"
+]
+
+stuDic["Name"] = "SuooL"
+
+stuDic
+
+let emptyArray = [String]()
+let emptyDic = [String : Double]()
+
+// for 循环
+
+for (item, content) in stuDic {
+    println("\(item) : \(stuDic[item]!)")
+}
 
 
+for num in 0...10 {
+    println("\(num)")
+}
+
+// 函数
+func greet(name: String, day: String) -> String {
+    return "Hello \(name), today is \(day)."
+}
+
+greet("Bob", "Tuesday")
+
+// 返回多个值
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+
+let statistics = calculateStatistics([5, 3, 100, 3, 9])
+statistics.sum
+statistics.2   // 也可以使用statistics.max
+statistics.1
+
+// 变长参数
+func sumOf(numbers: Int...) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    return sum
+}
+sumOf()
+sumOf(42, 597, 12)
+
+// 嵌套函数
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
+
+
+// 返回函数
+func makeIncrementer() -> (Int -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+// 函数传参
+func hasAnyMatches(list: [Int], condition: Int -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 12, 9]
+hasAnyMatches(numbers, lessThanTen)
+
+
+numbers.map({
+(number: Int) -> Int in
+    var result = 0
+    if number%2 != 0 {
+        result =  number
+    }
+    return result
+})
 
 
 
